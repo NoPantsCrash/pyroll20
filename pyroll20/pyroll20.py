2f5c849c554cd9e17717c7a1de4e26cb99ac33ff
george <abtziangiorgos@gmail.com>
2021-01-24 18:29:04 +0200

    Removed adding to individual rolls for now. Added Error handling. Cleaned up some code.

--- pyroll20/pyroll20.py
+++ pyroll20/pyroll20.py
@@ -2,67 +2,76 @@ from heapq import nlargest, nsmallest
 from random import randint
 import re
 
+dice_regex = re.compile(r"^(\d*)d(\d*)([hmle+t-])?(\d*)?$")
+
 
 #            'h',  # Highest Roll
 #            'l',  # Lowest Roll
 #            '+',  # Add to total
 #            '-',  # Remove from total
-#            '.+', # Add to every individual
-#            '.-', # Remove from every individual
+#            '.+', # Add to every individual TODO
+#            '.-', # Remove from every individual TODO
 #            'e',  # Exploding dice
 #            't',  # Total
 
 def roll(user_input: str = None):
-    dice_regex = re.compile(r"^(\d*)d(\d*)([hmle+t-][.+-]*)?(\d*)?$")
-    result = dice_regex.search(user_input)  # Searches through the user input for the roll
+    result = dice_regex.search(user_input)
+
     try:
         repeats = int(result.group(1))
-    except:
+    except AttributeError:  # In case a non integer was passed as a repeating argument
+        raise ValueError("Invalid entry")
+    except ValueError:  # if Nothing was provided, default to 1
         repeats = 1
-    sides = int(result.group(2))  # assign the sides of the die
-    modifier = []
     try:
-        modifier.append(result.group(3))
-        modifier.append(int(result.group(4)))
-    except:
-        pass
+        sides = int(result.group(2))  # assign the sides of the die
+    except ValueError:  # In case a non integer was passed as a repeating argument
+        raise ValueError("Invalid entry")
+
+    try:
+        modifier = result.group(3)
+    except ValueError:
+        modifier = None
+    try:
+        mod_value = int(result.group(4))
+    except ValueError:
+        mod_value = None
+
     if (int(sides) or int(repeats)) < 1:
         return "Invalid Parameter."
-    if repeats and sides:  # You can set a limit on how many sides or repeats, so to avoid hangups
-        if modifier[0] is not None:  # Checking for a modifier present
-            rolls = []
-            for i in range(repeats):
-                rolls.append(randint(1, sides))
-            rolls = moder(sides=sides, rolls=rolls, modifier=modifier)
-            return rolls
-        if modifier[0] is None:
-            rolls = []
-            for i in range(repeats):
-                rolls.append(randint(1, sides))
-            return rolls
+
+    if isinstance(modifier, str):  # Checking for a modifier present
+        rolls = []
+        for i in range(repeats):
+            rolls.append(randint(1, sides))
+        rolls = moder(sides=sides, rolls=rolls, modifier=modifier, mod_value=mod_value)
+        return rolls
     else:
-        return "Exceeds the roll limit."
+        rolls = []
+        for i in range(repeats):
+            rolls.append(randint(1, sides))
+        return rolls
 
 
-def moder(sides: int, rolls: list, modifier: list):
+def moder(sides: int, rolls: list, modifier: str, mod_value: int):
     if modifier[0] == 'e':
         return explode(sides=sides, rolls=rolls)  # Return exploded dice rolls along with one that were neutral
     if modifier[0] == 'h':  # Return highest
-        return nlargest(modifier[1], rolls)
+        return nlargest(mod_value, rolls)
     if modifier[0] == 'l':  # Return lowest
-        nsmallest(modifier[1], rolls)
+        return nsmallest(mod_value, rolls)
     if modifier[0] == '+':  # Add to sum of rolls
-        sum(rolls) + modifier[1]
+        return sum(rolls) + mod_value
     if modifier[0] == '-':  # Subtract from sum of rolls
-        return sum(rolls) - modifier[1]
-    if modifier[0] == '.-':  # Subtract from each roll
-        for i in range(0, len(rolls)):
-            rolls[i] = rolls[i] - modifier[1]
-        return rolls
-    if modifier[0] == '.+':  # Add to each roll
-        for i in range(0, len(rolls)):
-            rolls[i] = rolls[i] + modifier[1]
-        return rolls
+        return sum(rolls) - mod_value
+    # if modifier[0] == '.-':  # Subtract from each roll
+    #    for i in range(0, len(rolls)):
+    #        rolls[i] = rolls[i] - mod_value
+    #    return rolls
+    # if modifier[0] == '.+':  # Add to each roll
+    #    for i in range(0, len(rolls)):
+    #        rolls[i] = rolls[i] + mod_value
+    #    return rolls
     if modifier[0] == 't':  # Sum of rolls
         return sum(rolls)
 
@@ -80,5 +89,3 @@ def explode(sides: int, rolls: list):
                 if temp != sides:
                     loop = False
     return rolls
-
-
